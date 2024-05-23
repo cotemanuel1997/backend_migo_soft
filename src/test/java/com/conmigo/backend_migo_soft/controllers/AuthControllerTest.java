3a22f1154b42a78c9944434bfd0bff47be2940bc
emanuel cot <cotemanuel6@gmail.com>
2024-05-23 18:45:23 -0300

    add controller test for user registration

---
+++ src/test/java/com/conmigo/backend_migo_soft/controllers/AuthControllerTest.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.conmigo.backend_migo_soft.controllers;
+
+import com.conmigo.backend_migo_soft.login.controllers.AuthController;
+import com.conmigo.backend_migo_soft.login.models.ERole;
+import com.conmigo.backend_migo_soft.login.models.Role;
+import com.conmigo.backend_migo_soft.login.payload.request.SignupRequest;
+import com.conmigo.backend_migo_soft.login.repository.RoleRepository;
+import com.conmigo.backend_migo_soft.login.repository.UserRepository;
+import com.conmigo.backend_migo_soft.login.security.jwt.AuthEntryPointJwt;
+import com.conmigo.backend_migo_soft.login.security.jwt.AuthTokenFilter;
+import com.conmigo.backend_migo_soft.login.security.jwt.JwtUtils;
+import com.conmigo.backend_migo_soft.login.security.services.EmailService;
+import com.conmigo.backend_migo_soft.login.security.services.ForgotPasswordService;
+import com.conmigo.backend_migo_soft.login.security.services.UserDetailsServiceImpl;
+import java.util.Optional;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.csrf;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+/**
+ *
+ * @author emanuel
+ */
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+    
+    @Autowired
+    private MockMvc mvc;
+    
+    @Autowired
+    AuthenticationManager authenticationManager;
+    
+      @MockBean
+  UserRepository userRepository;
+
+  @MockBean
+  RoleRepository roleRepository;
+
+  @MockBean
+  PasswordEncoder encoder;
+
+  @MockBean
+  JwtUtils jwtUtils;
+  
+  @MockBean
+  ForgotPasswordService forgotPassService;
+  
+
+  @Autowired
+	private WebApplicationContext context;
+  @MockBean
+  
+  EmailService emailService;
+    	@Test
+       
+	public void testRegisterUserSuccess() throws Exception {
+            SignupRequest signupRequest = new SignupRequest();
+            signupRequest.setUsername("testuser");
+            signupRequest.setEmail("testuser@example.com");
+            signupRequest.setPassword("password");
+            signupRequest.setRole(Set.of("user"));
+            
+            mvc = MockMvcBuilders
+				.webAppContextSetup(context)
+				.apply(springSecurity()) 
+				.build();
+            
+                    //mockMvc = MockMvcBuilders
+                //.standaloneSetup(new AuthController())
+                //.build();
+            
+            when(userRepository.existsByUsername(signupRequest.getUsername())).thenReturn(false);
+            when(userRepository.existsByEmail(signupRequest.getEmail())).thenReturn(false);
+            when(roleRepository.findByName(ERole.ROLE_USER)).thenReturn(Optional.of(new Role(ERole.ROLE_USER)));
+            when(encoder.encode(signupRequest.getPassword())).thenReturn("encodedPassword");
+            
+           mvc.perform(post("/api/auth/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(signupRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("User registered successfully!"));
+
+          
+
+	}
+        
+    
+}
